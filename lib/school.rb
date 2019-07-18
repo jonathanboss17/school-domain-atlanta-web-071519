1ab1373d2c7f645b7c39d96461997168750babae
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 17:36:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -27,11 +27,11 @@ class School
     roster.size.times do |i|
       roster[i][keys[i]].sort
     end
-      
   end
   
 end
 
 cheese = {9 => ["AC Slater", "Zach Morris"], 10 => ["Aardvark", "Kelly Kapowski"], 11 => ["Screech", "Xavier"]}
-# keys = cheese.keys
+keys = cheese.keys
+
 
